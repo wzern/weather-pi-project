283dc2ac3dd4505020f04eaa64d592503185c7b1
William <me@wzernikow.com>
2022-09-15 09:16:42 +1200

    Add additional charts

--- frontend/index.php
+++ frontend/index.php
@@ -11,7 +11,15 @@
     <?php require_once 'includes/app.php'?>
 
     <div class="chart">
-        <canvas id="myChart"></canvas>
+        <canvas id="temperatureChart"></canvas>
+    </div>
+
+    <div class="chart">
+        <canvas id="humidityChart"></canvas>
+    </div>
+
+    <div class="chart">
+        <canvas id="pressureChart"></canvas>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -22,42 +30,87 @@
     const humidity = <?php echo json_encode($humidity) ?>;
     const pressure = <?php echo json_encode($pressure) ?>;
 
-    const data = {
+    const temperatureData = {
     labels: temperature,
     datasets: [{
         label: 'Temperature',
         data: temperature,
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgb(255, 99, 132)',
         tension: 0.1
-    },
-    {
+    }]};
+
+    const humidityData = {
+    labels: humidity,
+    datasets: [{
         label: 'Humidity',
         data: humidity,
         fill: false,
-        borderColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-    },
-    {
+    }]};
+
+    const pressureData = {
+    labels: pressure,
+    datasets: [{
         label: 'Pressure',
         data: pressure,
         fill: false,
         borderColor: 'rgb(153, 102, 255)',
         tension: 0.1
-    }]
-    };
+    }]};
 
     // Config Block
-    const config = {
+    const temperatureConfig = {
         type: 'line',
-        data
+        data: temperatureData,
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+    };
+
+    const humidityConfig = {
+        type: 'line',
+        data: humidityData,
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+    };
+
+    const pressureConfig = {
+        type: 'line',
+        data: pressureData,
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
     };
 
     // Render Block
-    const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
+    const temperatureChart = new Chart(
+        document.getElementById('temperatureChart'),
+        temperatureConfig
+    );
 
+    const humidityChart = new Chart(
+        document.getElementById('humidityChart'),
+        humidityConfig
+    );
+
+    const pressureChart = new Chart(
+        document.getElementById('pressureChart'),
+        pressureConfig
     );
     </script>
 
