36180a4b2593730483bfd184a5f9de6e323bc68b
William <me+github@wzernikow.com>
2022-09-18 17:12:27 +1200

    Fix incorrect readings of BMP180. New lib

--- esp8266/sensor-station-program/sensor-station-program.ino
+++ esp8266/sensor-station-program/sensor-station-program.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
-#include <SFE_BMP180.h>
-#include <Wire.h>
+#include <Adafruit_BMP085.h>
 #include "DHT.h"
 
 #define DHTTYPE DHT11   // DHT 11
@@ -12,15 +11,18 @@ const char* host = "Redacted";
 const char* api_key = "Redacted";
 const char* node_id = "Redacted";
 const int httpPort = 443;
-char status;
-double T, P, p0, a;
 
 DHT dht(dht_dpin, DHTTYPE);
-SFE_BMP180 pressure;
+Adafruit_BMP085 bmp;
 
 void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
+  
   dht.begin();
-  Serial.begin(115200);
   delay(10);
 
   Serial.println();
@@ -39,14 +41,6 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  if (pressure.begin())
-    Serial.println("BMP180 init success");
-  else
-  {
-    Serial.println("BMP180 init fail\n\n");
-    while (1); // Pause forever.
-  }
 }
 
 void loop() {
@@ -65,49 +59,7 @@ void loop() {
 
   float h = dht.readHumidity(); //Humidity level
   float t = dht.readTemperature(); //Temperature in celcius
-
-  status = pressure.getTemperature(T);
-  if (status != 0)
-  {
-    // Print out the measurement:
-    Serial.print("temperature: ");
-    Serial.print(T, 2);
-    Serial.print(" deg C, ");
-    Serial.print((9.0 / 5.0)*T + 32.0, 2);
-    Serial.println(" deg F");
-
-    // Start a pressure measurement:
-    // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
-    // If request is successful, the number of ms to wait is returned.
-    // If request is unsuccessful, 0 is returned.
-
-    status = pressure.startPressure(3);
-    if (status != 0)
-    {
-      // Wait for the measurement to complete:
-      delay(status);
-
-      // Retrieve the completed pressure measurement:
-      // Note that the measurement is stored in the variable P.
-      // Note also that the function requires the previous temperature measurement (T).
-      // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
-      // Function returns 1 if successful, 0 if failure.
-
-      status = pressure.getPressure(P, T);
-      if (status != 0)
-      {
-        // Print out the measurement:
-        Serial.print("absolute pressure: ");
-        Serial.print(P, 2);
-        Serial.print(" mb, ");
-        Serial.print(P * 0.0295333727, 2);
-        Serial.println(" inHg");
-      }
-      else Serial.println("error retrieving pressure measurement\n");
-    }
-    else Serial.println("error starting pressure measurement\n");
-  }
-  else Serial.println("error retrieving temperature measurement\n");
+  float p = bmp.readPressure();
 
   // We now create a URI for the request
   String url = "/weather-pi-project/backend/api/import.php/";
@@ -120,10 +72,9 @@ void loop() {
   url += "&humidity=";
   url += String(h);
   url += "&pressure=";
-  url += String(P, 2);
+  url += String(p/100);
   url += "&light=";
   url += "N/A";
-
 
   Serial.print("Requesting URL: ");
   Serial.println(url);
@@ -136,6 +87,5 @@ void loop() {
   Serial.println();
   Serial.println("closing connection");
 
-  
   delay(3600000);
 }
