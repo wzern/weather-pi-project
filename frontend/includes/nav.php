ca1d231480a8bec22e98eabd81b36c6084087b87
William Zernikow <me+github@wzernikow.com>
2022-09-20 11:43:52 +1200

    Seperate index into componenets

---
+++ frontend/includes/nav.php
@@ -0,0 +1,11 @@
+<?php session_start(); ?>
+
+<!-- Page navigation -->
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" id="closebtn" onclick="closeNav()"> &times; </a>
+  <a href="./">Dashboard</a>
+  <a href="./settings.php">Settings</a>
+  <a href=""></a>
+  <?php echo (isset($_SESSION['loggedin'])) ? "<a href='./logout.php'>Log Out</a>" : "<a href='./login.php'>Log In</a>";?>
+  
+</div>
