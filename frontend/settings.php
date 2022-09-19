df7904e820cdfe2d546cde0d93970dd060975ba3
William Zernikow <me+github@wzernikow.com>
2022-09-20 11:44:08 +1200

    Add settings page

---
+++ frontend/settings.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ./login.php?return=settings.php');
+	exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Page Title -->
+    <title>Settings | ⛅ ESPi Weather Station</title>
+
+    <!-- Dependencies -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <script src="scripts/nav.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+</head>
+<body>
+    <!-- Include site navigation -->
+    <?php require_once 'includes/nav.php'?>
+
+    <!-- Include main PHP script -->
+    <?php require_once 'includes/app.php'?>
+
+    <!-- Main body element -->
+    <main id="main">
+        <header>
+            <span id="openbtn" onclick="openNav()"> &#9776; </span>
+            <h1>⛅ ESPi Weather</h1>
+        </header>
+        
+        <div class="container" id="container">
+            Settings
+        </div>
+
+        <?php include 'includes/footer.html'; ?>
+    </main>
+
+    <!-- Render the charts with charts.js script -->
+    <script src="scripts/charts.js"></script>
+</body>
+</html>
