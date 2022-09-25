1633b3e9f87a7a248eea751ba3dda8757f2a85f2
William <me+github@wzernikow.com>
2022-09-26 01:40:41 +1300

    Fix SQL injection vuln

--- backend/api/import.php
+++ backend/api/import.php
@@ -14,28 +14,40 @@ $username = "username";
 $password = "password";
 $dbname = "weather-pi-project";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+
+$con = mysqli_connect($servername, $username, $password, $dbname);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT * FROM api_keys WHERE api_key = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $API_KEY);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
 
-$sql = "SELECT * FROM api_keys WHERE api_key='$API_KEY'";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0 && !empty($NODE_ID)) {
-    $sql = "INSERT INTO sensor_data_alt (node_id, temperature, humidity, pressure, light)
-    VALUES ('$NODE_ID', '$TEMPERATURE', '$HUMIDITY', '$PRESSURE', '$LIGHT')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Success";
-    } else {
-        echo "Fault: " . $sql . "<br>" . $conn->error;
+  if ($stmt->num_rows > 0) {
+    if ($stmt->num_rows > 0 && !empty($NODE_ID)) {
+      $sql = "INSERT INTO sensor_data_alt (node_id, temperature, humidity, pressure, light)
+      VALUES ('$NODE_ID', '$TEMPERATURE', '$HUMIDITY', '$PRESSURE', '$LIGHT')";
+  
+      if ($con->query($sql) === TRUE) {
+          echo "Success";
+      } else {
+          echo "Fault: " . $sql . "<br>" . $con->error;
+      }
+  
+      $stmt->close();
     }
+  } else {
+      // Incorrect username
+      echo 'Incorrect API key!';
+  }
 
-} else {
-  echo "Unauthorized";
+
+	$stmt->close();
 }
-$conn->close();
 ?> 
