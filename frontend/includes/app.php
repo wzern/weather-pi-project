c440086ce8edba2310f80d864067f8124eed150f
William Z <william@Williams-PC>
2022-09-27 21:52:33 +1300

    Fix overlapping error messages when no db con

--- frontend/includes/app.php
+++ frontend/includes/app.php
@@ -25,79 +25,79 @@ if ($resultNode->num_rows > 0) {
     while($row = $resultNode->fetch_assoc()) {
         $nodeIDArr[] = $row['node_id'];
     }
-} else {
-    echo "<center style='position: absolute; top: 10vh; left: 50%; transform: translateX(-50%)'>No devices were found</center>";
-}
 
-// Define a default option incase the user has not specified a node
-$defaultNodeID = (isset($nodeIDArr[0])) ? $nodeIDArr[0] : "Default";
-$activeNodeID = (!empty($_GET['node'])) ? $_GET['node'] : $defaultNodeID;
+    // Define a default option incase the user has not specified a node
+    $defaultNodeID = (isset($nodeIDArr[0])) ? $nodeIDArr[0] : "Default";
+    $activeNodeID = (!empty($_GET['node'])) ? $_GET['node'] : $defaultNodeID;
 
-// Define the differet time ranges
-$chartPeriods = ['Last 24 Hours', 'Last 48 Hours', 'Last 7 Days'];
-$selectedPeriod = $_GET['chartPeriod'];
+    // Define the differet time ranges
+    $chartPeriods = ['Last 24 Hours', 'Last 48 Hours', 'Last 7 Days'];
+    $selectedPeriod = $_GET['chartPeriod'];
 
-// Convert the selected time range to minutes by converting to hours, adding one hour, then subtracting 1 minute. 
-// This is so that the chart shows the last <x> amount of hours but also the current time.
-// E.g: Selected 24 hours, add one hour (25), convet to minutes (1500), subtract one minute -> 1499 minutes.
-// 1499 minutes on the chart might look something like 3pm yesterday to 3pm today rather than 4pm yesterday to 3pm today.
-if (!empty($selectedPeriod)) {
-    switch ($selectedPeriod) {
-        case 'Last 24 Hours':
-            $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
-            $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
-            break;
-        case 'Last 48 Hours':
-            $periodStr = 'time >= NOW() - INTERVAL 2939 MINUTE AND';
-            $timeFormatStr = 'timestampRawArr.map((i) => formatDayAMPM(roundHour(new Date(i))));';
-            break;
-        case 'Last 7 Days':
-            $periodStr = 'time >= NOW() - INTERVAL 10079 MINUTE AND';
-            $timeFormatStr = 'timestampRawArr.map((i) => formatDayAMPM(roundHour(new Date(i))));';
-            break;
-        default:
-            $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
-            $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
-      } 
-} 
+    // Convert the selected time range to minutes by converting to hours, adding one hour, then subtracting 1 minute. 
+    // This is so that the chart shows the last <x> amount of hours but also the current time.
+    // E.g: Selected 24 hours, add one hour (25), convet to minutes (1500), subtract one minute -> 1499 minutes.
+    // 1499 minutes on the chart might look something like 3pm yesterday to 3pm today rather than 4pm yesterday to 3pm today.
+    if (!empty($selectedPeriod)) {
+        switch ($selectedPeriod) {
+            case 'Last 24 Hours':
+                $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
+                $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
+                break;
+            case 'Last 48 Hours':
+                $periodStr = 'time >= NOW() - INTERVAL 2939 MINUTE AND';
+                $timeFormatStr = 'timestampRawArr.map((i) => formatDayAMPM(roundHour(new Date(i))));';
+                break;
+            case 'Last 7 Days':
+                $periodStr = 'time >= NOW() - INTERVAL 10079 MINUTE AND';
+                $timeFormatStr = 'timestampRawArr.map((i) => formatDayAMPM(roundHour(new Date(i))));';
+                break;
+            default:
+                $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
+                $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
+        } 
+    } 
 
-// Default to 24 hours
-else {
-    $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
-    $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
-}
+    // Default to 24 hours
+    else {
+        $periodStr = 'time >= NOW() - INTERVAL 1499 MINUTE AND';
+        $timeFormatStr = 'timestampRawArr.map((i) => formatAMPM(roundHour(new Date(i))));';
+    }
 
 
 
-// Execute SQL command
-$stmt = $conn->prepare('SELECT * FROM `sensor_data_alt` WHERE ' . $periodStr . ' node_id = ?');
-$stmt->bind_param('s', $activeNodeID); // 's' specifies the variable type => 'string'
-$stmt->execute();
+    // Execute SQL command
+    $stmt = $conn->prepare('SELECT * FROM `sensor_data_alt` WHERE ' . $periodStr . ' node_id = ?');
+    $stmt->bind_param('s', $activeNodeID); // 's' specifies the variable type => 'string'
+    $stmt->execute();
 
-$result = $stmt->get_result();
+    $result = $stmt->get_result();
 
-// Extract data into arrays if data exists
-if ($result->num_rows > 0) {
-    $temperatureDataArr = array();
-    $humidityDataArr = array();
-    $pressureDataArr = array();
-    $pressureSLDataArr = array();
-    $luxDataArr = array();
-    $timestampArr = array();
+    // Extract data into arrays if data exists
+    if ($result->num_rows > 0) {
+        $temperatureDataArr = array();
+        $humidityDataArr = array();
+        $pressureDataArr = array();
+        $pressureSLDataArr = array();
+        $luxDataArr = array();
+        $timestampArr = array();
 
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $temperatureDataArr[] = $row['temperature'];
-        $humidityDataArr[] = $row['humidity'];
-        $pressureDataArr[] = $row['pressure'];
-        $pressureSLDataArr[] = 1013.2;
-        $luxDataArr[] = $row['light'];
-        $luxDataMinAvgArr[] = 32000;
-        $luxDataMaxAvgArr[] = 100000;
-        $timestampArr[] = $row['time'];
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $temperatureDataArr[] = $row['temperature'];
+            $humidityDataArr[] = $row['humidity'];
+            $pressureDataArr[] = $row['pressure'];
+            $pressureSLDataArr[] = 1013.2;
+            $luxDataArr[] = $row['light'];
+            $luxDataMinAvgArr[] = 32000;
+            $luxDataMaxAvgArr[] = 100000;
+            $timestampArr[] = $row['time'];
+        }
+    } else {
+        echo "<center style='position: absolute; top: 10vh; left: 50%; transform: translateX(-50%)'>No data was found for this device</center>";
     }
 } else {
-    echo "<center style='position: absolute; top: 10vh; left: 50%; transform: translateX(-50%)'>No data was found for this device</center>";
+    echo "<center style='position: absolute; top: 10vh; left: 50%; transform: translateX(-50%)'>No devices were found</center>";
 }
 
 // Close Connection
