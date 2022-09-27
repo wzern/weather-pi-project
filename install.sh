9e642865cd09dbdb145f56d25e4c55cd8cd30d78
William Z <william@Williams-PC>
2022-09-27 20:32:26 +1300

    Fix phpMyAdmin installation block

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 YW=`echo "\033[33m"`
 RD=`echo "\033[01;31m"`
@@ -44,11 +43,12 @@ function msg_error() {
 }
 
 echo "  ______  _____ _____ _     _                _                  _ "
-echo " |  ____|/ ____|  __ (_)   | |    V1.0      | |  github/wzern  | |"
+echo " |  ____|/ ____|  __ (_)   | |    V1.1      | |  github/wzern  | |"
 echo " | |__  | (___ | |__) |    | |__   __ _  ___| | _____ _ __   __| |"
 echo " |  __|  \___ \|  ___/ |   | '_ \ / _' |/ __| |/ / _ \ '_ \ / _' |"
 echo " | |____ ____) | |   | |   | |_) | (_| | (__|   <  __/ | | | (_| |"
 echo " |______|_____/|_|   |_|   |_.__/ \__,_|\___|_|\_\___|_| |_|\__,_|"
+echo ""
 echo "------------------------------------------------------------------"
 echo ""
 echo "You are about to install the ESPi Backend: Stable"
@@ -77,7 +77,7 @@ apt-get install apache2 -y &>/dev/null
 msg_ok "Installed Apache2"
 
 msg_info "Installing MySQL"
-apt-get install mysql-server -y &>/dev/null
+apt-get install mariadb-server -y &>/dev/null
 msg_ok "Installed MySQL"
 
 msg_info "Installing PHP Modules"
@@ -87,10 +87,8 @@ msg_ok "Installed PHP Modules"
 msg_info "Installing phpMyAdmin"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -yq install phpmyadmin &>/dev/null
-cp ./conf/pma.conf /etc/dbconfig-common/phpmyadmin.conf &>/dev/null
+cp ./conf/phpmyadmin.conf /etc/dbconfig-common/phpmyadmin.conf &>/dev/null
 dpkg-reconfigure --frontend=noninteractive phpmyadmin &>/dev/null
-cp ./conf/phpmyadmin.conf /etc/apache2/conf-available/phpmyadmin.conf &>/dev/null
-cp ./conf/phpmyadmin.conf /etc/apache2/conf-enabled/phpmyadmin.conf &>/dev/null
 systemctl restart apache2 &>/dev/null
 msg_ok "Installed phpMyAdmin"
 
@@ -104,14 +102,23 @@ mysql -u root -e "CREATE DATABASE weather;"
 mysql -u root < conf/database.sql
 msg_ok "Prepared database"
 
-
 msg_info "Initialising web interface"
 rm -rf /var/www/html/* &>/dev/null
 cp -r frontend/* /var/www/html/ &>/dev/null
 cp -r backend/* /var/www/html/ &>/dev/null
+mkdir /var/www/html/phpmyadmin &>/dev/null
+cp -r /usr/share/phpmyadmin/* /var/www/html/phpmyadmin/ &>/dev/null
 msg_ok "Initialised web interface"
   
 msg_info "Cleaning up"
 apt-get autoremove >/dev/null
 apt-get autoclean >/dev/null
 msg_ok "Cleaned"
+
+echo ""
+echo "------------------------------------------------------------------"
+echo ""
+echo Server IP Address: $(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1) 
+echo ""
+echo "------------------------------------------------------------------"
+echo ""
